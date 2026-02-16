234e17f3b126578df1f1c80b9004c066f05d3d88
test-reservation326 <test.reservation326@gmail.com>
2026-02-16 14:47:20 +0900

    :)

---
+++ script.js
@@ -0,0 +1,21 @@
+async function GetCalenderUrl() {
+    const url = "https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec";
+    fetch(url).then(function (data) {
+        return data.json();
+    }).then(function (json) {
+        if (json.success) {
+
+        }
+    });
+}
+
+function getParameters() {
+
+}
+
+let reloadButton = document.getElementById('rb');
+reloadButton.onclick = doReload;
+
+function doReload() {
+    window.location.reload(false);
+}
