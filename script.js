5da59f4e20aef8b21c655d611ec4054b7db0d283
test-reservation326 <test.reservation326@gmail.com>
2026-02-17 14:56:28 +0900

    : )

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 async function GetCalenderUrl() {
-    const url = "https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec";
+    const userGrade = getParameter(userGrade);
+    const userClass = getParameter(userClass);
+
+    const url = `https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec?action=getCalender&userGrade=${userGrade}&userClass=${userClass}`;
     fetch(url).then(function (data) {
+        showLoadingAnimation();
         return data.json();
     }).then(function (json) {
+        hideLoadingAnimation();
         if (json.success) {
             let calenderUrl = json.calenderUrl;
         }
     });
 }
 
-function getParameters() {
-
+function getParameter(paramName) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(paramName);
 }
 
-function showDialog() {
-
+function showDialog(title, message) {
+    const dialog = document.getElementById("");
+    const dialogTitle = document.getElementById("");
+    const dialogMessage = document.getElementById("");
 }
 
 function hideDialog() {
-
+    const dialog = document.getElementById("");
+    const dialogTitle = document.getElementById("");
+    const dialogMessage = document.getElementById("");
 }
 
 function showLoadingAnimation() {
-
+    const loadingAnimation = document.getElementById("");
 }
 
 function hideLoadingAnimation() {
+    const loadingAnimation = document.getElementById("");
 
 }
+
+GetCalenderUrl();
 //(C) 2026 test.calender326
