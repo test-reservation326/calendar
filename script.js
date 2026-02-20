1b3b2508555a2425d6421a66693fbd434db3782a
test-reservation326 <test.reservation326@gmail.com>
2026-02-20 12:27:11 +0900

    test

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ async function getCalendarUrl() {
     const url = `https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec?action=getCalendar&userGrade=${userGrade}&userClass=${userClass}`;
     let userGrade = getParameter("userGrade");
     let userClass = getParameter("userClass");
-    fetch(url).then(function (data) {
-        showLoadingAnimation();
-        return data.json();
-    }).then(function (json) {
-        hideLoadingAnimation();
-        if (json.success) {
-            let calenderUrl = json.calenderUrl;
-        }
-    });
+    console.log(fetch(url));
+    // fetch(url).then(function (data) {
+    // showLoadingAnimation();
+    // return data.json();
+    // }).then(function (json) {
+    // hideLoadingAnimation();
+    // if (json.success) {
+    // let calenderUrl = json.calenderUrl;
+    // }
+    // });
 }
-getCalendarUrl();
+window.onload = getCalendarUrl();
 
 function getParameter(paramName) {
     const urlParams = new URLSearchParams(window.location.search);
