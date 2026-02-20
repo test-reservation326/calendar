e8bb9c7b415d0373226f0db6835e14632c0b899f
test-reservation326 <test.reservation326@gmail.com>
2026-02-20 13:51:33 +0900

    ? : (

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ async function getCalendarUrl() {
 
     if (userGrade && userClass) {
         const api = `https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec?action=getCalendar&userGrade=${userGrade}&userClass=${userClass}`;
-        const response = fetch(api);
-        const json = JSON.parse(response);
+        let response = fetch(api);
+        let json = JSON.parse(response);
         if (json.success) {
             console.log(json.url);
         }
