81bc99aac8187b4b6ecda117280d97df274d1a1c
test-reservation326 <test.reservation326@gmail.com>
2026-02-20 16:06:00 +0900

    fix

--- script.js
+++ script.js
@@ -5,18 +5,15 @@ async function getCalendarUrl() {
     if (userGrade && userClass) {
         const api = `https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec?action=getCalendar&userGrade=${userGrade}&userClass=${userClass}`;
         let json = fetch(api).then(res => res.json);
-
         if (json.success) {
             console.log(json.url);
-            var link = document.getElementById("link");
+            var link = document.getElementsByClassName("link");
             link.insertAdjacentHTML("beforeend", `<a href=${json.url};>get</a>`);
         }
     }
-
     //deleteParams
     const url = new URL(window.location.href);
     window.history.replaceState({}, '', url.pathname);
-
 }
 window.onload = getCalendarUrl();
 
