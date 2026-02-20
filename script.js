b23bde8408f8eec1d937eb2821f412c200af03fe
test-reservation326 <test.reservation326@gmail.com>
2026-02-20 13:48:32 +0900

    : >

--- script.js
+++ script.js
@@ -4,22 +4,17 @@ async function getCalendarUrl() {
 
     if (userGrade && userClass) {
         const api = `https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec?action=getCalendar&userGrade=${userGrade}&userClass=${userClass}`;
-        console.log(fetch(api));
+        const response = fetch(api);
+        const json = JSON.parse(response);
+        if (json.success) {
+            console.log(json.url);
+        }
     }
 
     //deleteParams
     const url = new URL(window.location.href);
     window.history.replaceState({}, '', url.pathname);
 
-    // fetch(url).then(function (data) {
-    // showLoadingAnimation();
-    // return data.json();
-    // }).then(function (json) {
-    // hideLoadingAnimation();
-    // if (json.success) {
-    // let calenderUrl = json.calenderUrl;
-    // }
-    // });
 }
 window.onload = getCalendarUrl();
 
