c36bebc56f5b30736e7ef2fed2499e30f57cb569
test-reservation326 <test.reservation326@gmail.com>
2026-02-22 14:21:10 +0900

    test

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ async function getCalendarUrl() {
 
     if (userGrade && userClass) {
         if (userGrade != 0 && userClass != 0) {
-            console.log(userGrade, userClass);
-            const api = `https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec?action=getCalendar&userGrade=${userGrade}&userClass=${userClass}`;
-            let json = fetch(api).then(res => res.json.results[0]);
-            console.log(json);
-
+            try {
+                const api = `https://script.google.com/macros/s/AKfycbx3zfsa6aJeL8ql97_q0EpxP1vvL7e5LJe_TYnucMHckyhZ_UIa_kz5obdE8NTOuCco/exec?action=getCalendar&userGrade=${userGrade}&userClass=${userClass}`;
+                const res = await fetch(api);
+                const json = await res.json();
+                console.log(json);
+            } catch (e) {
+                console.log(e);
+            }
         }
     }
     deleteParams();
